06b0039205dcdd669b06807953b527673e57ee73
Jose Manuel Marquez <jmarquezcamarillo@gmail.com>
2023-03-15 19:16:31 -0600

    primer commit

---
+++ src/main/java/com/kiubit/webapi/model/Usuario.java
@@ -0,0 +1,19 @@
+package com.kiubit.webapi.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    private String nombre;
+    private String apellidos;
+    private String email;
+    private String password;
+}
---
+++ src/main/java/com/kiubit/webapi/security/UserDetailServiceImpl.java
@@ -0,0 +1,23 @@
+package com.kiubit.webapi.security;
+
+import com.kiubit.webapi.model.Usuario;
+import com.kiubit.webapi.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findOneByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("El usuario con email " + email + "no existe."));
+        return new UserDetailsImpl(usuario);
+    }
+}
